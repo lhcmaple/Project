29d9678ecf2ad1498bcefe44c519c5afe5b4ab3c
lhcmaple <lhcmaple@gmail.com>
2020-11-01 21:37:01 +0800

    add class server

--- UDP/udp.cpp
+++ UDP/udp.cpp
@@ -1,9 +1,7 @@
 #include "udp.h"
 
-using namespace std;
-
 int main(int argc,char *argv[])
 {
-    
+
     return 0;
 }
--- UDP/udp.h
+++ UDP/udp.h
@@ -1,6 +1,18 @@
 #ifndef _H_UDP
 #define _H_UDP
 
+#include <sys/socket.h>
 
+class server{
+private:
+    int sockfd;
+    char *buff;
+    size_t nbytes;
+    int flags;
+    sockaddr from;
+    socklen_t addrlen;
+public:
+    server();
+};
 
 #endif
