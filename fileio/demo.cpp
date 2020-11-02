8193a03e5ba3e22eed0b426c04908e271243ef96
lhcmaple <lhcmaple@gmail.com>
2020-11-02 20:55:02 +0800

    normal commit

--- fileio/demo.cpp
+++ fileio/demo.cpp
@@ -2,6 +2,19 @@
 
 int main(int argc,char *argv[])
 {
-    
+    int fd1=open(argv[1],O_RDWR);
+    int fd2=open(argv[2],O_WRONLY|O_CREAT|O_TRUNC);
+    if(fd1<0||fd2<0)
+    {
+        cout<<"failed to open"<<endl;
+        return -1;
+    }
+    cout<<fd1<<endl;
+    char buf[1024];
+    int nbytes;
+    while(nbytes=read(fd1,buf,1024))
+    {
+        write(fd2,buf,nbytes);
+    }
     return 0;
 }
--- fileio/demo.h
+++ fileio/demo.h
@@ -1,6 +1,11 @@
 #ifndef _H_DEMO
 #define _H_DEMO
 
+#include <unistd.h>
+#include <fcntl.h>
 
+#include <iostream>
+
+using namespace std;
 
 #endif
